696fc0b48b53989b2433a7c29d591dd4f2943980
lHendralzz <hendraeffendi96@gmail.com>
2024-10-01 21:10:47 +0700

    fix : add order by in query in breaktime to order by start_time (#3)

--- models/breakTime.js
+++ models/breakTime.js
@@ -4,6 +4,9 @@ module.exports = class breakTime {
     constructor() {}
 
     static async fetchByDate(date) {
-        return db.query("SELECT * FROM break_time WHERE date = ?", [date]);
+        return db.query(
+            "SELECT id, start_time, end_time FROM break_time WHERE date = ? order by start_time",
+            [date]
+        );
     }
 };
